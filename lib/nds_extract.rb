14c41d8a853cafdba66b93e04b8473de224640de
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-02 07:25:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,6 +10,11 @@ while director_index < nds.length do
   totals [director_name] = 0 
   movie_index = 0 
   
-  
+  while_movie_index < nds[director_index][:movies].length do 
+    totals[director_name] += nds [director_index][:movies][:movie_index][:worldwide_gross]
+    movie_index += 1 
+  end 
+  director_index += 1 
 end
+
 end
